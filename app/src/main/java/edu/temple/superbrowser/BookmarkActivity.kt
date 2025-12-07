6d6ab2cd719352fec4e1485352ec4c00d24ff7ab
kevnugget <143677335+kevnugget@users.noreply.github.com>
2025-12-07 16:28:31 -0500

    Implement bookmark activity that handles the actual creation and deletion of bookmarks

--- app/src/main/java/edu/temple/superbrowser/BookmarkActivity.kt
+++ app/src/main/java/edu/temple/superbrowser/BookmarkActivity.kt
@@ -1,7 +1,67 @@
 package edu.temple.superbrowser
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class BookmarkActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        loadList(recyclerView)
+    }
+
+    private fun loadList(recyclerView: RecyclerView) {
+        val bookmarks = BookmarkManager.getBookmarks(this)
+
+        recyclerView.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+            inner class Holder(view: android.view.View) : RecyclerView.ViewHolder(view) {
+                val title = view.findViewById<TextView>(R.id.titleTv)
+                val delete = view.findViewById<ImageView>(R.id.deleteIv)
+            }
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+                return Holder(layoutInflater.inflate(R.layout.item_bookmark, parent, false))
+            }
+
+            override fun getItemCount() = bookmarks.size
+
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                val item = bookmarks[position]
+                (holder as Holder).title.text = item.title
+
+                holder.itemView.setOnClickListener {
+                    val resultIntent = Intent()
+                    resultIntent.putExtra("url", item.url)
+                    setResult(Activity.RESULT_OK, resultIntent)
+                    finish()
+                }
+
+                holder.delete.setOnClickListener {
+                    AlertDialog.Builder(this@BookmarkActivity)
+                        .setTitle("Delete Bookmark")
+                        .setMessage("Are you sure you want to delete ${item.title}?")
+                        .setPositiveButton("Yes") { _, _ ->
+                            BookmarkManager.deleteBookmark(this@BookmarkActivity, holder.adapterPosition)
+                            loadList(recyclerView) // Refresh the list
+                        }
+                        .setNegativeButton("No", null)
+                        .show()
+                }
+            }
+        }
+    }
 }
